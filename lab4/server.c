44ea7fe240d027ecbb069dbb1522cff1051a6159
Zihang Huang <zihang.huang@mail.utoronto.ca>
2019-04-01 14:27:46 -0400

    testing conflict in Lecture

--- lab4/server.c
+++ lab4/server.c
@@ -14,10 +14,10 @@
 #include <netdb.h>
 #include "server.h"
 #include "lookup_table.h"
+//////////////////////////////////
 
 
-
-
+/*********************************/
 
 
 // get sockaddr, IPv4 or IPv6:
@@ -26,7 +26,7 @@ void *get_in_addr(struct sockaddr *sa)
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
-
+//////////////////////////////////////
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
